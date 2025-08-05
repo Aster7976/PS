41fbc89f6bb6393c13f0341dc42e314da299df38
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2025-08-05 16:49:05 +0900

    Update Floyd.cpp

--- Floyd.cpp
+++ Floyd.cpp
@@ -2,27 +2,33 @@
 
 using namespace std;
 
-#define inf 1e8
+const int inf = 1e9;
+
 int d[101][101];
-int n, m;
+
 int main()
 {
-    ios_base::sync_with_stdio(0);
+    ios::sync_with_stdio(0);
     cin.tie(0);
 
+    int n, m;
     cin >> n >> m;
+
     for(int i = 1; i <= n; i++)
-        fill(d[i], d[i] + n + 1, inf);
-    
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            if(i != j)
+                d[i][j] = inf;
+        }
+    }
+
     for(int i = 0; i < m; i++)
     {
         int a, b, c;
         cin >> a >> b >> c;
         d[a][b] = min(d[a][b], c);
     }
-
-    for(int i = 1; i <= n; i++)
-        d[i][i] = 0;
 
     for(int i = 1; i <= n; i++)
     {
@@ -34,12 +40,12 @@ int main()
             }
         }
     }
-    
+
     for(int i = 1; i <= n; i++)
     {
         for(int j = 1; j <= n; j++)
         {
-            if(d[i][j] == inf) 
+            if(d[i][j] == inf)
                 cout << "0 ";
             else
                 cout << d[i][j] << ' ';
