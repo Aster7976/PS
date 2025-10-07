5de535e5b0cce20c0ff7e9229d2c13ee2bd6cec7
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2025-10-07 15:55:16 +0900

    Create Union Find (Disjoint Set).cpp

---
+++ Union Find (Disjoint Set).cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> p(1000001, -1);
+
+int find(int x)
+{
+    if(p[x] < 0)
+        return x;
+    p[x] = find(p[x]);
+    return find(p[x]);
+}
+
+bool uni(int u, int v)
+{
+    u = find(u);
+    v = find(v);
+    if(u == v)
+        return false;
+    p[v] = u;
+    return true;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    while(m--)
+    {
+        int op, a, b;
+        cin >> op >> a >> b;
+
+        if(op == 0)
+            uni(a, b);
+        else
+        {
+            if(find(a) == find(b))  
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        }
+    }
+}
