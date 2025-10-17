10a6993f4a59492904cff47eaf2344c6bb03535f
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2025-10-17 13:25:54 +0900

    Refactor Segment Tree to use int instead of ll

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -4,12 +4,10 @@
 
 using namespace std;
 
-typedef long long ll;
+vector<long long> v;
+vector<long long> tree;
 
-vector<ll> v;
-vector<ll> tree;
-
-void init(ll node, ll start, ll end)
+void init(int node, int start, int end)
 {
     if(start == end)
         tree[node] = v[start];
@@ -21,19 +19,19 @@ void init(ll node, ll start, ll end)
     }
 }
 
-ll query(ll node, ll start, ll end, ll left, ll right)
+long long query(int node, int start, int end, int left, int right)
 {
     if(left > end || right < start)
         return 0;
     if(left <= start && end <= right)
         return tree[node];
     
-    ll lsum = query(node * 2, start, (start + end) / 2, left, right);
-    ll rsum = query(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+    long long lsum = query(node * 2, start, (start + end) / 2, left, right);
+    long long rsum = query(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
     return lsum + rsum;
 }
 
-void update(ll node, ll start, ll end, ll idx, ll val)
+void update(int node, int start, int end, int idx, long long val)
 {
     if(idx < start || idx > end)
         return;
@@ -54,15 +52,15 @@ int main()
     ios::sync_with_stdio(0);
 	cin.tie(0);
 
-    ll n, m, k;
+    int n, m, k;
     cin >> n >> m >> k;
 
-    v = vector<ll>(n);
-    ll h = ll(ceil(log2(n)));
-    ll size = pow(2, (h + 1));
-    tree = vector<ll>(size);
+    v = vector<long long>(n);
+    int h = int(ceil(log2(n)));
+    int size = pow(2, (h + 1));
+    tree = vector<long long>(size);
 
-    for(ll i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
         cin >> v[i];
 
     init(1, 0, n - 1);
@@ -70,18 +68,18 @@ int main()
     m += k;
     while(m--)
     {
-        ll op;
+        int op;
         cin >> op;
         if(op == 1)
         {
-            ll idx;
-            ll val;
+            int idx;
+            long long val;
             cin >> idx >> val;
             update(1, 0, n - 1, idx - 1, val);
         }
         else
         {
-            ll left, right;
+            int left, right;
             cin >> left >> right;
             cout << query(1, 0, n - 1, left - 1, right - 1) << '\n';
         }
