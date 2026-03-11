8c474fa7b392b5d51a56f4d9bfc2c862f4b4b7c5
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-03-11 16:22:29 +0900

    Create Seive Of Eratosthenes.cpp

---
+++ Seive Of Eratosthenes.cpp
@@ -0,0 +1,51 @@
+// BOJ 1929
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define F first
+#define S second
+
+const int C = 1000001;
+
+bool prime[C];
+vector<int> v;
+
+void init()
+{
+    for(int i = 2; i < C; i++)
+        prime[i] = true;
+}
+
+void seive()
+{
+    for(int i = 2; i * i < C; i++)
+    {
+        if(prime[i])
+        {
+            for(int j = i * i; j < C; j += i)
+                prime[j] = false;
+        }
+    }   
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    init();
+    seive();
+
+    int m, n;
+    cin >> m >> n;
+
+    for(int i = m; i <= n; i++)
+    {
+        if(prime[i])
+            cout << i << '\n';
+    }
+}
