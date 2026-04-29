9312c5824fd93aa4cfc7f32d131d0a274bd2f121
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-04-29 16:33:42 +0900

    Update Seive Of Eratosthenes.cpp

--- Seive Of Eratosthenes.cpp
+++ Seive Of Eratosthenes.cpp
@@ -1,14 +1,3 @@
-// BOJ 1929
-
-#include <bits/stdc++.h>
-
-using namespace std;
-using ll = long long;
-using ld = long double;
-
-#define F first
-#define S second
-
 const int C = 1000001;
 
 vector<char> prime(C, true);
@@ -25,21 +14,4 @@ void seive()
                 prime[j] = false;
         }
     }   
-}
-
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    seive();
-
-    int m, n;
-    cin >> m >> n;
-
-    for(int i = m; i <= n; i++)
-    {
-        if(prime[i])
-            cout << i << '\n';
-    }
 }
