e4f5f9d127b717f3fae89e23a5bdc383f7f4e37c
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-04-29 17:51:39 +0900

    Add basic C++ template with standard includes

---
+++ Basic Template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+
+const ll INF = 2e18;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
