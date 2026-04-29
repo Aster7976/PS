5c6a4ef5cf86de9a38fd55b0637d4188b8a60684
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-04-29 16:32:33 +0900

    Refactor Mint class to use 'x' instead of 'v'

--- Mint.cpp
+++ Mint.cpp
@@ -1,131 +1,202 @@
-// BOJ 10869
-
-#include <bits/stdc++.h>
-
-using namespace std; 
-using ll = long long;
-using ld = long double;
-using pll = pair<ll, ll>;
-
-#define F first
-#define S second
-
-const ll INF = 2e18;
-
 template <ll MOD>
-class Mint {
+class Mint
+{
 public:
-    ll v;
+    ll x;
 
-    Mint() : v(0) {}
+    Mint() : x(0) {}
 
     template <typename T>
-    Mint(T x) {
-        v = (ll)(x % MOD);
-        if (v < 0) v += MOD;
+    Mint(T y)
+    {
+        x = (ll)(y % MOD);
+        if(x < 0)
+            x += MOD;
     }
 
-    static ll mod() { return MOD; }
+    ll mod() const
+    {
+        return MOD;
+    }
+    ll val() const
+    {
+        return x;
+    }
+    operator ll() const
+    {
+        return (ll)x;
+    }
+    operator int() const
+    {
+        return (int)x;
+    }
 
-    ll val() const { return v; }
-    explicit operator ll() const { return v; }
-    explicit operator int() const { return (int)v; }
-
-    Mint operator+() const { return *this; }
-    Mint operator-() const { return Mint(0) - *this; }
-
-    Mint& operator+=(const Mint& o) {
-        v += o.v;
-        if (v >= MOD) v -= MOD;
+    Mint operator+() const
+    {
         return *this;
     }
-    Mint& operator-=(const Mint& o) {
-        v -= o.v;
-        if (v < 0) v += MOD;
+    Mint operator-() const
+    {
+        return Mint(0) - *this;
+    }
+
+    Mint& operator+=(const Mint& y)
+    {
+        x += y.x;
+        if(x >= MOD)
+            x -= MOD;
         return *this;
     }
-    Mint& operator*=(const Mint& o) {
-        v = v * o.v % MOD;
+    Mint& operator-=(const Mint& y)
+    {
+        x -= y.x;
+        if(x < 0)
+            x += MOD;
         return *this;
     }
-    Mint& operator/=(const Mint& o) {
-        return *this *= o.inv();
+    Mint& operator*=(const Mint& y)
+    {
+        x = x * y.x % MOD;
+        return *this;
+    }
+    Mint& operator/=(const Mint& y)
+    {
+        return *this *= y.inv();
     }
 
-    Mint operator+(const Mint& o) const { return Mint(*this) += o; }
-    Mint operator-(const Mint& o) const { return Mint(*this) -= o; }
-    Mint operator*(const Mint& o) const { return Mint(*this) *= o; }
-    Mint operator/(const Mint& o) const { return Mint(*this) /= o; }
+    Mint operator+(const Mint& y) const
+    {
+        return Mint(*this) += y;
+    }
+    Mint operator-(const Mint& y) const
+    {
+        return Mint(*this) -= y;
+    }
+    Mint operator*(const Mint& y) const
+    {
+        return Mint(*this) *= y;
+    }
+    Mint operator/(const Mint& y) const
+    {
+        return Mint(*this) /= y;
+    }
 
-    Mint& operator++() { return *this += 1; }
-    Mint& operator--() { return *this -= 1; }
-    Mint operator++(int) { Mint t = *this; ++*this; return t; }
-    Mint operator--(int) { Mint t = *this; --*this; return t; }
+    Mint& operator++()
+    {
+        return *this += 1;
+    }
+    Mint& operator--()
+    {
+        return *this -= 1;
+    }
+    Mint operator++(int)
+    {
+        Mint t = *this;
+        ++*this;
+        return t;
+    }
+    Mint operator--(int)
+    {
+        Mint t = *this;
+        --*this;
+        return t;
+    }
 
-    bool operator==(const Mint& o) const { return v == o.v; }
-    bool operator!=(const Mint& o) const { return v != o.v; }
-    bool operator< (const Mint& o) const { return v <  o.v; }
-    bool operator<=(const Mint& o) const { return v <= o.v; }
-    bool operator> (const Mint& o) const { return v >  o.v; }
-    bool operator>=(const Mint& o) const { return v >= o.v; }
+    bool operator==(const Mint& y) const
+    {
+        return x == y.x;
+    }
+    bool operator!=(const Mint& y) const
+    {
+        return x != y.x;
+    }
+    bool operator<(const Mint& y) const
+    {
+        return x < y.x;
+    }
+    bool operator<=(const Mint& y) const
+    {
+        return x <= y.x;
+    }
+    bool operator>(const Mint& y) const
+    {
+        return x > y.x;
+    }
+    bool operator>=(const Mint& y) const
+    {
+        return x >= y.x;
+    }
 
-    Mint inv() const {
-        ll a = v, b = MOD, x = 1, y = 0;
-        while (b) {
+    Mint inv() const
+    {
+        ll a = x;
+        ll b = MOD;
+        ll c = 1;
+        ll d = 0;
+
+        while(b)
+        {
             ll q = a / b;
-            a -= q * b; swap(a, b);
-            x -= q * y; swap(x, y);
+            a -= q * b;
+            swap(a, b);
+            c -= q * d;
+            swap(c, d);
         }
-        return Mint(x);
+
+        return Mint(c);
     }
 
-    Mint pow(ll e) const {
-        if (e < 0) return inv().pow(-e);
-        Mint base = *this, result = 1;
-        for (; e > 0; e >>= 1) {
-            if (e & 1) result *= base;
+    Mint pow(ll y) const
+    {
+        if(y < 0)
+            return inv().pow(-y);
+
+        Mint base = *this;
+        Mint result = 1;
+
+        while(y > 0)
+        {
+            if(y & 1)
+                result *= base;
             base *= base;
+            y >>= 1;
         }
+
         return result;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Mint& m) {
-        return os << m.v;
+    friend std::ostream& operator<<(std::ostream& out, const Mint& y)
+    {
+        return out << y.x;
     }
-    friend std::istream& operator>>(std::istream& is, Mint& m) {
-        ll x; is >> x; m = Mint(x);
-        return is;
+    friend std::istream& operator>>(std::istream& in, Mint& y)
+    {
+        ll a;
+        in >> a;
+        y = Mint(a);
+        return in;
     }
 
-    template <typename T>
-    friend Mint operator+(T l, const Mint& r) { return Mint(l) + r; }
-    template <typename T>
-    friend Mint operator-(T l, const Mint& r) { return Mint(l) - r; }
-    template <typename T>
-    friend Mint operator*(T l, const Mint& r) { return Mint(l) * r; }
-    template <typename T>
-    friend Mint operator/(T l, const Mint& r) { return Mint(l) / r; }
+    template<typename T>
+    friend Mint operator+(T l, const Mint& r)
+    {
+        return Mint(l) + r;
+    }
+    template<typename T>
+    friend Mint operator-(T l, const Mint& r)
+    {
+        return Mint(l) - r;
+    }
+    template<typename T>
+    friend Mint operator*(T l, const Mint& r)
+    {
+        return Mint(l) * r;
+    }
+    template<typename T>
+    friend Mint operator/(T l, const Mint& r)
+    {
+        return Mint(l) / r;
+    }
 };
 
 using mint = Mint<998244353>;
-
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    ll a, b;
-    cin >> a >> b;
-
-    mint c = a;
-    mint d = b;
-
-    c += 998244353;
-    d += 998244353;
-
-    cout << c + d << '\n';
-    cout << a - b << '\n';
-    cout << c * d << '\n';
-    cout << a / b << '\n';
-    cout << a % b << '\n';
-}
