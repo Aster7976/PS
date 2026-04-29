00cc2c0978af17285840eedb3233db8526cba107
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-04-29 17:50:51 +0900

    Extract Dijkstra's algorithm into a function
    Refactored Dijkstra's algorithm into a separate function for better modularity.

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,32 +1,9 @@
-// BOJ 1753
-
-#include <bits/stdc++.h>
-
-using namespace std;  
-
-#define F first
-#define S second
-
-const int INF = 1e9;
-
 int v, e, start;
 vector<pair<int, int>> adj[20001];
 vector<int> dist(20001, INF);
 
-int main()
+void dijkstra()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> v >> e >> start;
-
-    while(e--)
-    {
-        int u, v, w;
-        cin >> u >> v >> w;
-        adj[u].push_back({w, v});
-    }
-
     priority_queue<pair<int, int>,
     vector<pair<int, int>>,
     greater<pair<int, int>>> pq;
@@ -39,8 +16,6 @@ int main()
         auto cur = pq.top();
         pq.pop();
 
-        // F 거리, S 정점 번호
-
         if(dist[cur.S] != cur.F)
             continue;
 
@@ -52,13 +27,5 @@ int main()
             dist[next.S] = dist[cur.S] + next.F;
             pq.push({dist[next.S], next.S});
         }
-    }
-
-    for(int i = 1; i <= v; i++)
-    {
-        if(dist[i] == INF)
-            cout << "INF\n";
-        else
-            cout << dist[i] << '\n';
     }
 }
