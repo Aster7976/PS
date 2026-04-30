7d06c4d5bf9a574f28f00c70088da8e796e12493
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-04-30 22:30:53 +0900

    Update Floyd.cpp

--- Floyd.cpp
+++ Floyd.cpp
@@ -1,59 +1,35 @@
-// BOJ 11404
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
-const int inf = 1e9;
-
-int d[101][101];
-
-int main()
+void floyd()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n, m;
-    cin >> n >> m;
-
-    for(int i = 1; i <= n; i++)
+    for(ll i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= n; j++)
+        for(ll j = 1; j <= n; j++)
         {
             if(i != j)
-                d[i][j] = inf;
+                dist[i][j] = INF;
         }
     }
 
-    for(int i = 0; i < m; i++)
+    for(ll i = 1; i <= n; i++)
     {
-        int a, b, c;
-        cin >> a >> b >> c;
-        d[a][b] = min(d[a][b], c);
+        for(ll j = 1; j <= n; j++)
+        {   
+            cin >> dist[i][j];
+            nxt[i][j] = j;
+        }
     }
 
-    for(int i = 1; i <= n; i++)
+    for(ll i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= n; j++)
+        for(ll j = 1; j <= n; j++)
         {
-            for(int k = 1; k <= n; k++)
+            for(ll k = 1; k <= n; k++)
             {
-                d[j][k] = min(d[j][k], d[j][i] + d[i][k]);
+                if(dist[j][k] > dist[j][i] + dist[i][k])
+                {
+                    dist[j][k] = dist[j][i] + dist[i][k];
+                    nxt[j][k] = nxt[j][i];
+                }
             }
         }
     }
-
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= n; j++)
-        {
-            if(d[i][j] == inf)
-                cout << "0 ";
-            else
-                cout << d[i][j] << ' ';
-        }
-        cout << '\n';
-    }
 }
-
-
