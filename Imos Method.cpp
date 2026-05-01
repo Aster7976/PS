6d7d4699bc4dc3de7cc1a17fb4834dff99609c9a
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-05-01 16:38:55 +0900

    Update Imos Method.cpp

--- Imos Method.cpp
+++ Imos Method.cpp
@@ -1,46 +1,19 @@
-//BOJ 28018
+ll n;
+vector<ll> v(n);
 
-#include <bits/stdc++.h>
-
-using namespace std;
-using ll = long long;
-using ld = long double;
-
-#define F first
-#define S second
-
-int main()
+void imos()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int n;
-    cin >> n;
-
-    vector<int> imos(1000001);
-
     while(n--)
     {
-        int l, r;
+        ll l, r;
         cin >> l >> r;
 
         r++;
         
-        imos[l]++;
-        imos[r]--;
+        v[l]++;
+        v[r]--;
     }
 
-    for(int i = 2; i <= 1000000; i++)
-        imos[i] += imos[i - 1];
-
-    int q;
-    cin >> q;
-
-    while(q--)
-    {
-        int t;
-        cin >> t;
-
-        cout << imos[t] << '\n';
-    }
+    for(ll i = 2; i <= n; i++)
+        v[i] += v[i - 1];
 }
