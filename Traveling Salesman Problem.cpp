91972811280a94a7239e77af8cfd9c354bc0cb93
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-05-01 17:12:58 +0900

    Update Traveling Salesman Problem.cpp

--- Traveling Salesman Problem.cpp
+++ Traveling Salesman Problem.cpp
@@ -1,17 +1,17 @@
-// BOJ 2098
+ll n;
+ll board[n][n];
+ll dp[n][1 << n];
 
-#include <bits/stdc++.h>
+void init()
+{
+    for(int i = 0; i < 16; i++)
+    {
+        for(int j = 0; j < 1 << 16; j++)
+            dp[i][j] = -1;
+    }
+}
 
-using namespace std;
-
-const int INF = 1e9;
-
-int board[16][16];
-int dp[16][1 << 16];
-
-int n;
-
-int tsp(int cur, int visited)
+ll tsp(ll cur, ll visited)
 {
     if(visited == (1 << n) - 1)
     {
@@ -21,13 +21,13 @@ int tsp(int cur, int visited)
             return INF;
     }
 
-    int& ret = dp[cur][visited];
+    ll& ret = dp[cur][visited];
 
     if(ret != -1)
         return ret;
 
     ret = INF;
-    for(int next = 0; next < n; next++)
+    for(ll next = 0; next < n; next++)
     {
         if(!(visited & (1 << next)) && board[cur][next] != 0)
         {
@@ -36,32 +36,4 @@ int tsp(int cur, int visited)
     }
 
     return ret;
-}
-
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> n;
-
-    for(int i = 0; i < 16; i++)
-    {
-        for(int j = 0; j < 1 << 16; j++)
-        {
-            dp[i][j] = -1;
-        }
-    }
-
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            cin >> board[i][j];
-        }
-    }
-
-    int ans = tsp(0, 1);
-
-    cout << ans;
 }
