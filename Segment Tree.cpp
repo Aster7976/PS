c017fa1a6ce15e8dfb3a3401f9ee44040dd626d6
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-05-01 17:20:53 +0900

    Refactor Segment Tree code for consistency and clarity

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,13 +1,8 @@
-// BOJ 2042
+ll n;
+vector<ll> v(n);
+vector<ll> tree(4 * n);
 
-#include <bits/stdc++.h>
-
-using namespace std;
-
-vector<long long> v;
-vector<long long> tree;
-
-void init(int node, int start, int end)
+void init(ll node, ll start, ll end)
 {
     if(start == end)
 	{
@@ -20,19 +15,19 @@ void init(int node, int start, int end)
 	tree[node] = tree[node * 2] + tree[node * 2 + 1];
 }
 
-long long query(int node, int start, int end, int left, int right)
+ll query(ll node, ll start, ll end, ll left, ll right)
 {
     if(left > end || right < start)
         return 0;
     if(left <= start && end <= right)
         return tree[node];
     
-    long long lsum = query(node * 2, start, (start + end) / 2, left, right);
-    long long rsum = query(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+    ll lsum = query(node * 2, start, (start + end) / 2, left, right);
+    ll rsum = query(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
     return lsum + rsum;
 }
 
-void update(int node, int start, int end, int idx, long long val)
+void update(ll node, ll start, ll end, ll idx, ll val)
 {
     if(idx < start || idx > end)
         return;
@@ -46,43 +41,4 @@ void update(int node, int start, int end, int idx, long long val)
     update(node * 2, start, (start + end) / 2, idx, val);
     update(node * 2 + 1, (start + end) / 2 + 1, end, idx, val);
     tree[node] = tree[node * 2] + tree[node * 2 + 1];
-}
-
-int main()
-{
-    ios::sync_with_stdio(0);
-	cin.tie(0);
-
-    int n, m, k;
-    cin >> n >> m >> k;
-
-    v.resize(n);
-    int h = (int)ceil(log2(n));
-    int s = 1 << (h + 1);
-    tree.resize(s);
-
-    for(int i = 0; i < n; i++)
-        cin >> v[i];
-
-    init(1, 0, n - 1);
-
-    m += k;
-    while(m--)
-    {
-        int op;
-        cin >> op;
-        if(op == 1)
-        {
-            int idx;
-            long long val;
-            cin >> idx >> val;
-            update(1, 0, n - 1, idx - 1, val);
-        }
-        else
-        {
-            int left, right;
-            cin >> left >> right;
-            cout << query(1, 0, n - 1, left - 1, right - 1) << '\n';
-        }
-    }
 }
