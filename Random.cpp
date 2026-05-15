3433e80df538d714d63ca39bedb58049439e2fec
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-05-15 17:20:41 +0900

    Update Random.cpp

--- Random.cpp
+++ Random.cpp
@@ -1,4 +1,4 @@
-    std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());  
-    std::uniform_int_distribution<ll> dist(l, r);
+std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());  
+std::uniform_int_distribution<ll> dist(l, r);
 
-    cout << dist(rng) << ' ';
+cout << dist(rng) << ' ';
