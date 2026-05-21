36c932f1615f7d27bc2795b6047ad400c080c800
Aster7976 <117650329+Aster7976@users.noreply.github.com>
2026-05-21 09:32:17 +0900

    Create Combinations and Permutations.cpp

---
+++ Combinations and Permutations.cpp
@@ -0,0 +1,32 @@
+void init()
+{
+    fact[0] = 1;
+
+    for(ll i = 1; i < n; i++)
+        fact[i] = fact[i - 1] * i;
+
+    ifact[n - 1] = fact[n - 1].inv();
+
+    for(ll i = n - 2; i >= 0; i--)
+        ifact[i] = ifact[i + 1] * (i + 1);
+}
+
+mint nCr(ll x, ll y)
+{
+    return fact[x] * ifact[y] * ifact[x - y];
+}
+
+mint nPr(ll x, ll y)
+{
+    return fact[x] * ifact[x - y];
+}
+
+mint nHr(ll x, ll y)
+{
+    return nCr(x + y - 1, y);
+}
+
+mint nPIr(mint x, ll y)
+{
+    return x.pow(y);
+}
